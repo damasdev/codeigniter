7e509d7034214f62761a907eefd9ac51232c3998
damasdev <damas@majoo.id>
2022-04-06 11:19:11 +0700

    update composer and environment

--- application/hooks/Environment.php
+++ application/hooks/Environment.php
@@ -6,5 +6,7 @@ class Environment
     {
         $dotenv = Dotenv\Dotenv::createUnsafeMutable(FCPATH . "../");
         $dotenv->load();
+
+        $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
     }
 }
