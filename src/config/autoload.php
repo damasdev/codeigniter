17fb7e427b69ec35e1ad8b44f55c4a14ce603944
damasdev <damas@majoo.id>
2022-04-08 11:09:10 +0700

    remove migration from autoload

--- src/config/autoload.php
+++ src/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('twig', 'session', 'database', 'form_validation', 'ion_auth', 'migration');
+$autoload['libraries'] = array('twig', 'session', 'database', 'form_validation', 'ion_auth');
 
 /*
 | -------------------------------------------------------------------
